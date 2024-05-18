3fa06eb2b586579dfaf9cb2e236fe4904cebe5ba
Projects <artur_kulembetov@outlook.com>
2024-05-18 19:16:13 +0700

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import React from "react";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Weather Wise",
+  description:
+    "An application that displays the weather forecast and current weather conditions based on the user-selected location.",
+  keywords: "weather, climate, temperature",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html className="dark" lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
---
+++ eslint.config.js
@@ -0,0 +1,40 @@
+const reactPlugin = require("eslint-plugin-react");
+const typescriptPlugin = require("@typescript-eslint/eslint-plugin");
+const prettierPlugin = require("eslint-plugin-prettier");
+const typescriptParser = require("@typescript-eslint/parser");
+
+module.exports = [
+  {
+    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: typescriptParser,
+      sourceType: "module",
+    },
+    plugins: {
+      react: reactPlugin,
+      "@typescript-eslint": typescriptPlugin,
+      prettier: prettierPlugin,
+    },
+    rules: {
+      "prettier/prettier": "error",
+      "react/react-in-jsx-scope": "off",
+      "@typescript-eslint/no-unused-vars": ["error"],
+      "no-console": "warn",
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  {
+    files: ["**/*.js", "**/*.jsx"],
+    languageOptions: {
+      globals: {
+        browser: true,
+        es2021: true,
+        node: true,
+      },
+    },
+  },
+];
