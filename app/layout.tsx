7a9ff33bbbfac409ce6b72617b540309cb17a590
Projects <artur_kulembetov@outlook.com>
2024-05-19 19:18:59 +0700

    build: remove unused react-toastify from layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import React from "react";
-import "react-toastify/dist/ReactToastify.css";
 import { ThemeProvider } from "@/contexts/ThemeContext";
 import { ToastProviderWrapper } from "@/hooks/useToast";
 
